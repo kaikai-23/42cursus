1b42386c8996981a7804ed7a34553343ef58bdad
Homon Kai <hkai@c1r7s1.42tokyo.jp>
2023-10-01 13:09:55 +0900

    ft_strncmp.c のセグフォの原因が解決できてない

--- Libft_00/ft_strncmp.c
+++ Libft_00/ft_strncmp.c
@@ -1,25 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkai <hkai@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 12:26:28 by hkai              #+#    #+#             */
+/*   Updated: 2023/10/01 13:08:05 by hkai             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    unsigned char   str1;
-    unsigned char   str2;
+	unsigned char	str1;
+	unsigned char	str2;
 
-    while((*s1 || *s2) && (0 < n--))
-    {
-        str1 = (unsigned char)*s1;
-        str2 = (unsigned char)*s2;
-        if (str1 != str2)
-        {
-            if ((unsigned char)*s1 > (unsigned char)*s2)
-                return (1);
-            else
-                return (-1);
-        }
-        s1++;
-        s2++;
-    }
-        return (0);
+	while ((*s1 || *s2) && (0 < n--))
+	{
+		str1 = (unsigned char)*s1;
+		str2 = (unsigned char)*s2;
+		if (str1 != str2)
+			return (str1 - str2);
+		s1++;
+		s2++;
+	}
+	return (0);
 }
 
 // #include <stdio.h>
