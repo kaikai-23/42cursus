453347c735be2d0726347c7586599cbaf8e3effc
kaikai-23 <gate.sea0203@gmail.com>
2023-10-03 23:59:26 +0900

    ft_itoa.c 問題に対応できた

--- Libft_00/ft_itoa.c
+++ Libft_00/ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkai <hkai@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: takaramonkai <takaramonkai@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 17:02:48 by hkai              #+#    #+#             */
-/*   Updated: 2023/10/03 19:42:46 by hkai             ###   ########.fr       */
+/*   Updated: 2023/10/03 23:58:38 by takaramonka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 
 #include "libft.h"
 
+static char	*special_value(int n)
+{
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+	else if (n == 0)
+		return (ft_strdup("0"));
+	return (NULL);
+}
+
 static int	digit_count(int n)
 {
 	int	count;
 
-	if (n == INT_MIN)
-		return (11);
 	count = 0;
 	if (n < 0)
 	{
@@ -37,12 +44,16 @@ static int	digit_count(int n)
 
 //getconf INT_MAX : 2147483647
 //getconf INT_MIN : -2147483648
-//mallocでINT_MINに対応したい
+//mallocでINT_MINに対応したい→strdup
 char	*ft_itoa(int n)
 {
 	char	*str;
 	int		len;
 
+	if (n == INT_MIN || n == 0)
+		return (special_value(n));
+	// if (n == INT_MIN)
+	// 	return (ft_strdup("-2147483648"));
 	len = digit_count(n);
 	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
@@ -50,16 +61,11 @@ char	*ft_itoa(int n)
 	str[len] = '\0';
 	if (n < 0)
 	{
-		if (n == INT_MIN)
-		{
-			str[--len] = '8';
-			n /= 10;
-		}
 		str[0] = '-';
 		n *= (-1);
 	}
-	if (n == 0)
-		str[0] = '0';
+	// if (n == 0)
+	// 	str[0] = '0';
 	while (n > 0)
 	{
 		str[len - 1] = (n % 10) + '0';
@@ -72,10 +78,12 @@ char	*ft_itoa(int n)
 // #include <stdio.h>
 // int main()
 // {
+// 	printf("result : %s\n", ft_itoa(-2));
+// 	printf("result : %s\n", ft_itoa(2));
 // 	printf("result : %s\n", ft_itoa(-2147483648));
-// // 	printf("result : %s\n", ft_itoa(2147483647));
-// // 	printf("result : %s\n", ft_itoa(-2147483647));
-// 	// printf("result : %s\n", ft_itoa(0));
-// // 	// printf("result : %s\n", ft_itoa(a));
-// // 	// printf("result : %s\n", ft_itoa(2147483648));
+// 	printf("result : %s\n", ft_itoa(2147483647));
+// 	printf("result : %s\n", ft_itoa(-2147483647));
+// 	printf("result : %s\n", ft_itoa(0));
+// 	// printf("result : %s\n", ft_itoa(a));
+// 	// printf("result : %s\n", ft_itoa(2147483648));
 // }
