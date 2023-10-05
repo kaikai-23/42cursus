ac4a9a28b2526491d516785c6218022fabd01457
Homon Kai <hkai@c1r19s7.42tokyo.jp>
2023-10-05 16:51:59 +0900

    ft_lstadd_front.c やっとポインタの動きを理解した

--- Libft_00/ft_lstadd_front.c
+++ Libft_00/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: hkai <hkai@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 16:16:11 by hkai              #+#    #+#             */
-/*   Updated: 2023/10/04 16:58:01 by hkai             ###   ########.fr       */
+/*   Updated: 2023/10/05 16:51:04 by hkai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,39 +25,40 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-
-	new->next = *lst;
-	*lst = new;
+	//lst[0]は*lstでも良い、とにかくリストにおける最初の要素のアドレスである
+	//具体的な値（lstのnextとか）ではないので注意が必要だよ
+	new->next = lst[0];
+	//アドレスをアドレスで更新してるよ
+	lst[0] = new;
 }
 
+// #include <stdio.h>
 
-#include <stdio.h>
+// int main()
+// {
+//     t_list *head = NULL; // リストの先頭へのポインタを初期化
+//     t_list *new_node;
 
-int main()
-{
-    t_list *head = NULL; // リストの先頭へのポインタを初期化
-    t_list *new_node;
+//     // リストに最初のノード（背番号1）を追加
+//     new_node = ft_lstnew((void *)1);
+//     ft_lstadd_front(&head, new_node);
 
-    // リストに最初のノード（背番号1）を追加
-    new_node = ft_lstnew((void *)1);
-    ft_lstadd_front(&head, new_node);
+//     // リストの先頭に新しいノード（背番号10）を追加
+//     new_node = ft_lstnew((void *)10);
+//     ft_lstadd_front(&head, new_node);
 
-    // リストの先頭に新しいノード（背番号10）を追加
-    new_node = ft_lstnew((void *)10);
-    ft_lstadd_front(&head, new_node);
+//     // リストの先頭に新しいノード（背番号20）を追加
+//     new_node = ft_lstnew((void *)20);
+//     ft_lstadd_front(&head, new_node);
 
-    // リストの先頭に新しいノード（背番号20）を追加
-    new_node = ft_lstnew((void *)20);
-    ft_lstadd_front(&head, new_node);
+//     // 確認のためにリストを表示
+//     t_list *current = head;
+//     while (current != NULL)
+//     {
+//         printf("背番号: %d\n", (int)current->content);
+//         current = current->next;
+//     }
 
-    // 確認のためにリストを表示
-    t_list *current = head;
-    while (current != NULL)
-    {
-        printf("背番号: %d\n", (int)current->content);
-        current = current->next;
-    }
-
-    // TODO: リストのメモリを解放
-    return 0;
-}
+//     // TODO: リストのメモリを解放
+//     return 0;
+// }
