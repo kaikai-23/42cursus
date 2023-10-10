bedeeb001f12a01de41e40e6d48a3db200149502
Homon Kai <hkai@c1r24s11.42tokyo.jp>
2023-10-10 13:47:41 +0900

    修正

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hkai <hkai@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 18:46:14 by hkai              #+#    #+#             */
-/*   Updated: 2023/10/08 20:56:36 by hkai             ###   ########.fr       */
+/*   Updated: 2023/10/10 13:43:47 by hkai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_calloc(size_t count, size_t size)
 	if (size && count > SIZE_MAX / size)
 		return (NULL);
 	//freeするために0の時は1だけ確保してあげる
-	if (n == 0)
+	if (count == 0 || size == 0)
 		n = 1;
 	else
 		n = count * size;
