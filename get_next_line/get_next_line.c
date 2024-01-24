0d6c19456ea60b6f8ae5fa4cfe6bc5d154d0954f
kaikai-23 <gate.sea0203@gmail.com>
2024-01-25 01:09:13 +0900

    必須項目ほぼ完成

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,112 +1,9 @@
-// ssize_t用
-#include <unistd.h>
+#include "get_next_line.h"
 
-// printf用
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// BUFFER_SIZEが定義されていなければ5で定義
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 5
-#endif
-
-// Leakのチェック
-__attribute__((destructor))
-static void destructor() {
-    system("leaks -q a.out");
-}
-
-size_t	ft_strlen(const char *str)
-{
-	size_t	length;
-
-	length = 0;
-	while (str[length] != '\0')
-		length++;
-	return (length);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	char	after_convert;
-
-	after_convert = (char)c;
-	while (*s && *s != after_convert)
-		s++;
-	if (*s == after_convert)
-		return ((char *)s);
-	else
-		return (NULL);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	count;
-	size_t	i;
-
-	count = 0;
-	while (src[count] != '\0')
-		count++;
-	if (dstsize == 0)
-		return (count);
-	i = 0;
-	while (src[i] != '\0' && i < dstsize - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (count);
-}
-
-
-char	*ft_strdup(const char *s1)
-{
-	size_t		size;
-	char		*arr;
-	size_t		i;
-
-	size = 0;
-	size = ft_strlen(s1);
-	arr = malloc(sizeof(char) * (size + 1));
-	if (!arr)
-		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		arr[i] = s1[i];
-		i++;
-	}
-	arr[i] = '\0';
-	return (arr);
-}
-
-
-// char	*ft_strjoin(char const *s1, char const *s2)
-// {
-// 	char	*res;
-// 	int		i;
-// 	int		j;
-// 	int sizetotal = ft_strlen(s1) + ft_strlen(s2);
-// 	i = 0;
-// 	res = malloc(sizeof(char) * (sizetotal + 1));
-// 	if (!res || !s1 || !s2)
-// 		return (NULL);
-// 	while (s1[i] != 0)
-// 	{
-// 		res[i] = s1[i];
-// 		i++;
-// 	}
-// 	j = 0;
-// 	while (s2[j] != 0)
-// 	{
-// 		res[i] = s2[j];
-// 		i++;
-// 		j++;
-// 	}
-// 	res[sizetotal] = 0;
-// 	return (res);
+// function for Leak
+// __attribute__((destructor))
+// static void destructor() {
+//     system("leaks -q a.out");
 // }
 
 char	*ft_strjoin(char const *s1, char const *s2)
@@ -140,36 +37,35 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (str);
 }
 
-char *get_oneline(char *store, int *count_p)
+
+
+
+// まずはすべてを読み込んで返す関数を作ってみる
+char    *get_next_line(int fd)
 {
-	char *oneline;
 	
-    // 処理後ファイルが終わりに達した場合はNULLを返す
-    if (!store || store[0] == '\0')
-	{
-        // ここでstoreはfreeしちゃだめ（次のところでfreeするから）
-        store = NULL;
-        return NULL;
+	// 改行が見つかるまで読み込んだ行を保持し続ける変数
+	static char *store;
+	char		*oneline;
+	int	count = 0;
+
+	// 無効な識別子
+    if (fd < 0 || BUFFER_SIZE <= 0)
+    {
+        write(1, "無効なファイルディスクリプタorバッファサイズ", 42);
+        return (NULL);
     }
-
-	// *count_p = 0;
-	while (store[*count_p] && store[*count_p] != '\n')
-		*count_p = *count_p + 1;
-	
-	// 出力するためのoneline。\nと\0の2つ分を考慮してコピー
-	if (store[*count_p] && store[*count_p] == '\n')
+	store = read_line(fd, store);
+	if (!store)
 	{
-		oneline = strndup(store, (*count_p + 2));
-		oneline[*count_p + 1] = '\0';
+		free(store);
+		return NULL;
 	}
-	else
-	{
-		// 改行見つからないケース
-		oneline = strndup(store, (*count_p + 1));
-		oneline[*count_p] = '\0';
-	}
+	oneline = get_oneline(store, &count);
 
-	return (oneline);
+	store = find_next_line(store, &count);
+
+    return (oneline);
 }
 
 // read_line関数(readしたものをstoreに保存)
@@ -182,7 +78,7 @@ char	*read_line(int fd, char *store)
 
 	// 最初だけstore確保
 	if (!store)
-		store = strdup("");
+		store = ft_strdup("");
 	// BUFFER_SIZEが大きくなるとbufに残ったりするから動的確保
 	buf = calloc(BUFFER_SIZE+1, sizeof(char));
 
@@ -211,6 +107,38 @@ char	*read_line(int fd, char *store)
 	return (store);
 }
 
+
+char *get_oneline(char *store, int *count_p)
+{
+	char *oneline;
+	
+    // 処理後ファイルが終わりに達した場合はNULLを返す
+    if (!store || store[0] == '\0')
+	{
+        // ここでstoreはfreeしちゃだめ（次のところでfreeするから）
+        store = NULL;
+        return NULL;
+    }
+
+	// *count_p = 0;
+	while (store[*count_p] && store[*count_p] != '\n')
+		*count_p = *count_p + 1;
+	oneline = ft_calloc(*count_p + 2, sizeof(char));
+	*count_p = 0;
+	while (store[*count_p] && store[*count_p] != '\n')
+	{
+		oneline[*count_p] = store[*count_p];
+		*count_p = *count_p + 1;
+	}
+
+	// 出力するためのoneline。\nと\0の2つ分を考慮してコピー
+	if (store[*count_p] && store[*count_p] == '\n')
+		oneline[*count_p] = '\n';
+
+	return (oneline);
+}
+
+
 char *find_next_line(char *store, int *count_p)
 {
 	// ファイル終端
@@ -233,32 +161,4 @@ char *find_next_line(char *store, int *count_p)
 	}
 	free(store);
 	return (temp);
-}
-
-// まずはすべてを読み込んで返す関数を作ってみる
-char    *get_next_line(int fd)
-{
-	
-	// 改行が見つかるまで読み込んだ行を保持し続ける変数
-	static char *store;
-	char		*oneline;
-	int	count = 0;
-
-	// 無効な識別子
-    if (fd < 0 || BUFFER_SIZE <= 0)
-    {
-        write(1, "無効なファイルディスクリプタorバッファサイズ", 42);
-        return (NULL);
-    }
-	store = read_line(fd, store);
-	if (!store)
-	{
-		free(store);
-		return NULL;
-	}
-	oneline = get_oneline(store, &count);
-
-	store = find_next_line(store, &count);
-
-    return (oneline);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,9 +1,25 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
+// BUFFER_SIZEが定義されていなければ5で定義
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 5
+#endif
+
+// size_t
 #include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 char *get_next_line(int fd);
+char	*read_line(int fd, char *store);
+char *get_oneline(char *store, int *count_p);
+char *find_next_line(char *store, int *count_p);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif
